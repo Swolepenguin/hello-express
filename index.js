f1e9a7eb223043cb9c58f92578f2be1bff9fea9e
swolepenguin <ashtonm223@gmail.com>
2020-11-13 12:41:01 -0500

    adding paths about and blog

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const express = require('express')
+const express = require('express');
+const res = require('express/lib/response');
 const app = express();
 
 app.get('/', function (req, res){
     res.send('hello,sei wprld')
 });
 
+app.get('/about', function(req, res){
+    res.send('yerrrrrrr')
+});
+app.get('/blog',function(req,res){
+    res.send('you know what it is')
+});
+
 app.listen(8000);
+
+
